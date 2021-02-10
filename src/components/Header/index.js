7cd08008f615d0663508b2a75d66cca459475f51
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-10 18:57:44 +0700

    [UPDATE]Header Use Tailwincss

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -10,62 +10,69 @@ import { capitalizeFirstLetter, getLocalStorage } from '../../helper';
 
 import '../../style/style.css';
 
-class Header extends Component {
-  constructor(props){
-    super(props);
-    this.goBack = this.goBack.bind(this);
-  };
 
-  componentDidMount() {
-    this.props.getUser();
-  };
-  
-  _renderContent() {
-    if(getLocalStorage('profile')[1]){
+const Header = (props) => {
+  useEffect(() => {
+    if(!props.getUser) {
+      props.getUser();
+    }
+    console.log('mount it!');
+  }, [props]);
+
+  const _goBack = useCallback(
+    () => {
+      props.history.goBack();
+    },
+    [props]
+  );
+
+  const _renderContent = useCallback(
+    () => {
+      if(getLocalStorage('profile')[1]){
+        return (
+          <Link to="/login" className="picture-profile">
+            <img src={getLocalStorage('profile')[1].userData.picture} alt="user" />
+          </Link>
+        );
+      };
       return (
         <Link to="/login" className="picture-profile">
-          <img src={getLocalStorage('profile')[1].userData.picture} alt="user" />
+          <img src={blankUserImage} alt="user-blank" />
         </Link>
       );
-    };
-    return (
-      <Link to="/login" className="picture-profile">
-        <img src={blankUserImage} alt="user-blank" />
-      </Link>
-    );
-  };
+    },
+    []
+  );
 
-  goBack(){
-    this.props.history.goBack();
-  };
-
-  _conditionHeader(path) {
-    if(path === 'budget') {
+  const _conditionHeader = useCallback(
+    (path) => {
+      if(path === 'budget') {
+        return (
+          <div className="wrapper-header">
+            <div className="back-button"></div>
+            <div className="header-title white">D-Budget</div>
+            {_renderContent()}
+          </div>
+        );
+      };
+  
       return (
         <div className="wrapper-header">
-          <div className="back-button"></div>
-          <div className="header-title white">D-Budget</div>
-          {this._renderContent()}
+          <div onClick={_goBack} className="back-button">
+            <img src={backArrow} alt='' />
+          </div>
+          <div className="header-title white">
+            {capitalizeFirstLetter(path)}
+          </div>
+          {_renderContent()}
         </div>
-      );
-    };
-    return (
-      <div className="wrapper-header">
-        <div onClick={this.goBack} className="back-button">
-          <img src={backArrow} alt='' />
-        </div>
-        <div className="header-title white">
-          {capitalizeFirstLetter(path)}
-        </div>
-        {this._renderContent()}
-      </div>
-    );
-  };
+      );    
+    }, 
+    [_goBack, _renderContent]
+  );
   
-  render() {
-    const path = this.props.location.pathname.slice(1);
-    return this._conditionHeader(path);
-  };
+  const path = props.location.pathname.slice(1);
+  return _conditionHeader(path);
 };
 
 export default connect(({ user }) => ({ user }), actions)(withRouter(Header));
