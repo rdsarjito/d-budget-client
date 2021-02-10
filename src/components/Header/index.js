d2d66c3f0a9119aaebcae9411745cbc5fa2e33e5
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-10 23:27:39 +0700

    [UPDATE]Fixing Bug Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ import backArrow from '../../images/back-arrow.png';
 import blankUserImage from '../../images/blank-user.png';
 
 import * as actions from '../../actions';
-import { getLocalStorage } from '../../helper';
+import { capitalizeFirstLetter, getLocalStorage } from '../../helper';
 
 
 import '../../style/style.css';
@@ -35,7 +35,27 @@ const Header = (props) => {
       if(getLocalStorage('profile')[1]){
         return (
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <img className="h-8 w-8 rounded-full" src={getLocalStorage('profile')[1].userData.picture} alt="user" />
+            <button onClick={() => setIsOpen(!isOpen)} className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" >
+              <span className="sr-only">Open user menu</span>
+              <img className="h-8 w-8 rounded-full" src={getLocalStorage('profile')[1].userData.picture} alt="user" />
+            </button>
+            <Transition
+            show={isOpen}
+            enter="transition ease-out duration-100 transform"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-75 transform"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            {(ref) => (
+              <div ref={ref} className="rounded-md bg-white shadow-xs mt-8">
+                <div className="mt-2 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5">
+                  <Link to="/logout" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Log Out</Link>
+                </div>
+              </div>
+            )}
+          </Transition>
           </div>
         );
       };
@@ -135,7 +155,7 @@ const Header = (props) => {
       <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 class="text-3xl font-bold leading-tight text-gray-900">
-            {path}
+            {capitalizeFirstLetter(path)}
           </h1>
         </div>
       </header>
