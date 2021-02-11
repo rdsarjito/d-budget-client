31b68cf7eb5c63908a45f04b8ad9236119ce3a9e
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-11 13:28:58 +0700

    [ADD]Style Nav Mobile

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -8,8 +8,8 @@ import '../../style/style.css';
 class Menu extends Component {
   render() {
     return (
-      <div className="w-60 mx-auto absolute inset-x-0 bottom-11 bg-white-800 md:hidden lg:hidden">
-        <div className="flex justify-between">
+      <div className="w-9/12 mx-auto absolute inset-x-0 bottom-11 bg-white-800 md:hidden lg:hidden">
+        <div className="flex justify-around border border-gray-300 rounded shadow-md">
           <Buttonadd /> 
         </div>
       </div>
