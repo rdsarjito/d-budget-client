30a8cb0e08f355d70765666f3c19a70b58d8ee97
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-18 21:42:41 +0700

    [UPDATE]Component to Hook Balance

--- src/containers/Balance/index.js
+++ src/containers/Balance/index.js
@@ -1,17 +1,16 @@
-import { Component } from 'react';
+import React from 'react';
 
 import TotalBalance from '../../components/TotalBalance';
 
 import '../../style/style.css';
 
-class Balance extends Component {
-  render() {
-    return (
-      <div className="w-40 mx-auto">
-        <TotalBalance income={this.props.income} expense={this.props.expense} />
-      </div>
-    )
-  }
-}
+const Balance = () => {
+  return (
+    <div className="w-40 mx-auto">
+      <TotalBalance income={this.props.income} expense={this.props.expense} />
+    </div>
+  );
+};
+
 
 export default Balance;
