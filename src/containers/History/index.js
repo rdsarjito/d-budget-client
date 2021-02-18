a1af3cdbdd70dfb676d9ba062381d57c84385b94
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-18 21:40:22 +0700

    [UPDATE]Component to Hook History

--- src/containers/History/index.js
+++ src/containers/History/index.js
@@ -1,17 +1,15 @@
-import { Component } from 'react';
+import React from 'react';
 
 import HistoryRow from '../../components/HistoryRow';
 
 import '../../style/style.css';
 
-class History extends Component {
-  render() {
-    return (
-      <div className="history-container">
-        <HistoryRow />
-      </div>
-    )
-  }
-}
+const History = () => {
+  return (
+    <div className="history-container">
+      <HistoryRow />
+    </div>
+  );
+};
 
 export default History;
