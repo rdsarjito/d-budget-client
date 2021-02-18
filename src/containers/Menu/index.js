3c212f20a115d13949cf46d42f776d4828566e7e
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-18 21:41:41 +0700

    [UPDATE]Component to Hook Menu

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -1,20 +1,19 @@
-import { Component } from 'react';
+import React from 'react';
 
 import Buttonadd from '../../components/ButtonMenu';
 
 import '../../style/style.css';
 
 
-class Menu extends Component {
-  render() {
-    return (
-      <div className="w-80 mx-auto absolute inset-x-0 bottom-11 bg-white-800 md:hidden lg:hidden">
-        <div className="ml-4 mr-4 flex justify-around border border-gray-300 rounded pt-3 pb-3 shadow-md">
-          <Buttonadd /> 
-        </div>
+const Menu = () => {
+  return (
+    <div className="w-80 mx-auto absolute inset-x-0 bottom-11 bg-white-800 md:hidden lg:hidden">
+      <div className="ml-4 mr-4 flex justify-around border border-gray-300 rounded pt-3 pb-3 shadow-md">
+        <Buttonadd /> 
       </div>
-    )
-  }
-}
+    </div>
+  )
+};
+
 
 export default Menu;
