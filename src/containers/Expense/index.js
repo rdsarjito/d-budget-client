f7b89bc7232ed3f5429e5419142e345ac4e76c4a
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-18 21:38:50 +0700

    [UPDATE]Component to Hook Exepsen

--- src/containers/Expense/index.js
+++ src/containers/Expense/index.js
@@ -1,27 +1,27 @@
-import { Component } from 'react';
-import { connect } from 'react-redux'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
 
-import * as actions from '../../actions';
+import { getTransaction } from '../../actions';
 import FormTransaction from '../../components/FormTransaction';
 import ListData from '../../components/ListData';
 import TotalCashFlow from '../../components/TotalCashFlow';
 
 const API = `http://localhost:5000`;
 
-class Expense extends Component {
-  componentDidMount(){
-    this.props.getTransaction('expense');
-    this.props.getCategory();
-  };
-  render() {
-    return (
-      <main>
-        <FormTransaction type="expense" API={API} />
-        <TotalCashFlow type="expense" />
-        <ListData type="expense" API={API} />
-      </main>
-    );
-  };
+const Expense = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTransaction('expense'))
+  }, [dispatch]);
+
+  return (
+    <main>
+      <FormTransaction type="expense" API={API} />
+      <TotalCashFlow type="expense" />
+      <ListData type="expense" API={API} />
+    </main>
+  );
 };
 
-export default connect(null, actions)(Expense);
+export default Expense;
